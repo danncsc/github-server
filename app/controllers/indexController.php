8fd44fe56ea3dd932608b34c4e41a025b9013efe
yoyo930021 <yoyo930021@gmail.com>
2015-08-22 22:15:23 +0800

    修正

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -45,6 +45,14 @@ class indexController extends BaseController {
 
 	public function control(){
 		$url = "https://api.github.com/users/danncsc/repos";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+		$data = curl_exec($ch);
+		curl_close($ch);
 		$data = file_get_contents($url); // 取得json字串
 		$data = json_decode($data, true); // 將json字串轉成陣列
 		return View::make('control')->with('projects',$data);
